4cfd189fed2129c8786e8edb59d7d14e91a33ca1
iadducchio3 <isaac@adducchio.com>
2019-02-21 13:17:57 -0500

    Threading update

--- Python Scripts/MQTT_Controller.py
+++ Python Scripts/MQTT_Controller.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-'''
 import time
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import datetime
 import SMS
-from threading import Thread
-from datetime import timedelta
+#from threading import Thread
+#from datetime import timedelta
 
 hvac_power = 'off'
 last_temp = 0
@@ -18,11 +15,11 @@ last_home_motion = datetime.datetime.now()
 
 # FUNCTION| RUNS WHEN CLIENT CONNECTS TO SERVER
 
-def on_connect(client,userdata,flags,rc,):
+def on_connect(client, userdata, flags, rc,):
     print 'Connected with result code ' + str(rc)
 
 
-def controlHVAC():
+'''def controlHVAC():
     global hvac_power, last_temp, last_temp_set
     if hvac_power == 'on':
         if last_temp_set < last_temp:
@@ -30,10 +27,10 @@ def controlHVAC():
         elif last_temp_set > last_temp:
             client.publish('home/fan_power', 'off')
     elif hvac_power == 'off':
-        client.publish('home/fan_power', 'off')
+        client.publish('home/fan_power', 'off')'''
 
 
-def automaticLights():
+'''def automaticLights():
     print("Thread Started")
     global client
     while 1:
@@ -44,7 +41,7 @@ def automaticLights():
             time.sleep(difference)
         else:
             client.publish('home/light_power', 'off')
-    print('exiting automatic lights')
+    print('exiting automatic lights')'''
 
 
 # FUNCTION| RUNS WHEN MESSAGE IS RECEIVED
@@ -93,21 +90,21 @@ def on_message(client, userdata, msg):
 
 
 # CREATE "CLIENT" TO CONNECT TO LOCAL SERVER
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect('localhost', 1883, 60)
+client = mqtt.Client() 
+client.on_connect = on_connect 
+client.on_message = on_message 
+client.connect('localhost', 1883, 60) 
 
-newThread = Thread(target=automaticLights, args=())
+'''newThread = Thread(target=automaticLights, args=())
 newThread.daemon = True
-newThread.start()
+newThread.start()'''
 
 # CREATE THREAD TO PROCESS MESSAGE QUEUE
 client.loop_start()
 
 # SUBSCRIBE TO ALL TOPICS
 
-client.subscribe('#')
+client.subscribe("#")
 #client.subscribe('home/temperature_power')
 #client.subscribe('home/temperature_set')
 #client.subscribe('home/temperature_sensor')
@@ -120,105 +117,3 @@ client.subscribe('#')
 
 while 1:
     time.sleep(10000)
-'''
-import time 
-import RPi.GPIO as GPIO 
-import paho.mqtt.client as mqtt 
-import paho.mqtt.publish as publish
-import datetime
-import SMS
-
-temperature = 70
-light_intensity = 50
-temperature_set = 70
-security_power = 'off'
-blinds = 'closed'
-temperature_power = 'off'
-lights = 'off'
-
-
-# FUNCTION| RUNS WHEN CLIENT CONNECTS TO SERVER
-def on_connect(client, userdata, flags, rc): 
-  print("Connected with result code " + str(rc)) 
-
-
-def newTemp(value):
-  temperature = value
-
-
-def newTempSet(value):
-  temperature_set = value
-
-
-def newBlind(value):
-  blinds = value
-
-
-def newTempPower(value):
-  temperature_power = value
-
-
-def newLightPower(value):
-  lights = value
-
-
-def newLightIntensity(value):
-  light_intensity = value
-
-def newSecurityPower(value):
-  security_power = value
-
-
-# FUNCTION| RUNS WHEN MESSAGE IS RECEIVED
-def on_message(client, userdata, msg):
-   #TEMPORARY TO SEE WHATS COMING INTO BROKER 
-  new_message = msg.topic
-  message_payload = msg.payload
-  #CHECK THE TOPIC OF THE MESSAGE
-  if new_message == 'home/temperature_power':
-    print('Temperature Status: '+msg.payload)
-    newTempPower(message_payload)
-  elif new_message == 'home/light_power':
-    print('Light Status: '+msg.payload)
-    newLightPower(message_payload)
-  elif new_message == 'home/security_power':
-    print('Security Status: '+msg.payload)
-    newSecurityPower(message_payload)
-  elif new_message == 'home/light_sensor':
-    print('Light Intensity: '+msg.payload)
-    newLightIntensity(message_payload)
-  elif new_message == 'home/temperature_set':
-    print('Temperature Setting: '+msg.payload)
-    newTempSet(message_payload)
-  elif new_message == 'home/blind_status':
-    print('Blind Status: '+msg.payload)
-    newBlind(message_payload)
-  elif new_message == 'home/temperature_sensor':
-    print('Temperature: '+msg.payload)
-    newTemp(value)
-  elif new_message == 'home/motion':
-   	dt = datetime.datetime.now()
-   	time = dt.strftime("%d %b %Y")+" | "+ dt.strftime("%I:%M %p")
-   	print('Motion Detected: '+ time)
-   	client.publish("home/last_motion_detected",time)
-   	if security_power == 'on':
-   		send_message("Motion Detected")
- 
-
-
-
-
-# CREATE "CLIENT" TO CONNECT TO LOCAL SERVER
-client = mqtt.Client() 
-client.on_connect = on_connect 
-client.on_message = on_message 
-client.connect('localhost', 1883, 60) 
-
-#CREATE THREAD TO PROCESS MESSAGE QUEUE 
-client.loop_start() 
-
-#SUBSCRIBE TO ALL TOPICS 
-client.subscribe("#")
-
-while 1:
-  time.sleep(1000)
