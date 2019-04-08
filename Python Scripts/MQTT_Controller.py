e348fe4ede7653840f4ce1485e4e4a6007301b13
iadducchio3 <isaac@adducchio.com>
2019-04-08 19:08:05 -0400

    troubleshooting text messaging

--- Python Scripts/MQTT_Controller.py
+++ Python Scripts/MQTT_Controller.py
@@ -82,7 +82,7 @@ def on_message(client, userdata, msg):
         time = dt.strftime('%d %b %Y') + ' | ' + dt.strftime('%I:%M %p')
         #print 'Motion Detected: ' + time
         client.publish('home/last_motion_detected', time)
-        SMS.send_message('Motion detected at: '+String(time))
+        SMS.send_message('Motion detected at: %s ' % time)
         client.publish('home/security_light', 'on')
     elif new_message == 'home/home_motion':
         last_home_motion = datetime.datetime.now()
